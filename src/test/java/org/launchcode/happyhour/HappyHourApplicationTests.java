b13bc7aabcffb68798169c16f21f1bd64ff0446d
cllefler <catherinelefler@yahoo.com>
2020-08-14 06:06:00 -0500

    first commit

---
+++ src/test/java/org/launchcode/happyhour/HappyHourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.happyhour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
